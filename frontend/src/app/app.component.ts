98bd22e8963d4b59fbc89354c7215c44eded2425
Ася <massikanastasia@gmail.com>
2025-03-16 16:20:56 +0200

    Removed old vscode config files

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    NavbarComponent, 
+    RouterModule, 
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Macrame';
+}
